5a34dab99064cf14f40fb7111f4391ed9b4ca0af
Tazkia Athariza Dhivara <tazkia.athariza.dhivara-2018@fst.unair.ac.id>
2022-04-08 17:55:39 +0700

    finished capacity filter

--- public/scripts/app.example.js
+++ public/scripts/app.example.js
@@ -3,7 +3,7 @@ class App {
     this.clearButton = document.getElementById("clear-btn");
     this.loadButton = document.getElementById("load-btn");
     this.carContainerElement = document.getElementById("cars-container");
-    this.penumpang = document.getElementById("penumpang").value;
+    this.jumlahPenumpang = document.getElementById("jumlahPenumpang");
   }
 
   async init() {
@@ -22,20 +22,20 @@ class App {
     });
   };
 
-  async loadFilter(penumpang) {
-    console.log('penumpang', penumpang);
+  async loadFilter(jumlahPenumpang) {
+    console.log('jumlahPenumpang', jumlahPenumpang);
     const cars = await Binar.listCars((item) => {
-     return item.capacity >= Number(penumpang);
+     return item.capacity == Number(jumlahPenumpang);
     });
     Car.init(cars);
   }
 
-  async load() {
+  /* async load() {
     const cars = await Binar.listCars((item) => {
      return item.available === true;
     });
     Car.init(cars);
-  } 
+  } */
 
   clear = () => {
     let child = this.carContainerElement.firstElementChild;
--- public/scripts/main.example.js
+++ public/scripts/main.example.js
@@ -13,13 +13,17 @@ console.log(params);
  * Contoh penggunaan DOM di dalam class
  * */
 const app = new App();
-app.loadButton.onclick = app.loadFilter(app.penumpang.value);
+app.loadButton.onclick = app.loadFilter(app.jumlahPenumpang.value);
 console.log(app.loadButton);
-console.log(app.penumpang.value);
+console.log(app.jumlahPenumpang.value);
 const btn_load = app.loadButton;
 btn_load.addEventListener ('click', function(e) {
     e.preventDefault();
-    console.log(app.penumpang);
-    app.loadFilter(app.penumpang).then(app.run);
+    console.log(app.jumlahPenumpang.value);
+    app.loadFilter(app.jumlahPenumpang.value).then(app.run);
+})
+const btn_clear = app.clearButton;
+btn_clear.addEventListener ('click', function(){
+    location.reload()
 })
 // app.init().then(app.run);
